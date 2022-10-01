e72badb2998adc4ce82ebc7b962c9e61b8d95b31
Danish <danish.shoukat@gagamuller.com>
2022-10-01 17:04:43 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { AddproductComponent } from './Components/addproduct/addproduct.component';
+import { SalesReportComponent } from './Components/sales-report/sales-report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"login",component:LoginComponent},
+  {path:"dashboard",component:DashboardComponent, children:[
+    {
+      path:"products", component:ProductsComponent
+    },
+    {
+      path:"addproduct", component:AddproductComponent
+    },
+    {
+      path:"salesReport", component:SalesReportComponent
+    }
+  ]},
+  // {path:"products",component:ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { AddproductComponent } from './Components/addproduct/addproduct.component';
+import { SalesReportComponent } from './Components/sales-report/sales-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    ProductsComponent,
+    AddproductComponent,
+    SalesReportComponent
   ],
   imports: [
     BrowserModule,
